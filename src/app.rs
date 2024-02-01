19ea2efdf52b9c47d797816e8d89f32fa169969a
noxware <7684329+noxware@users.noreply.github.com>
2024-02-01 20:16:38 -0300

    resizable and draggable iframe window

--- src/app.rs
+++ src/app.rs
@@ -1,30 +1,74 @@
-pub struct TemplateApp;
+use egui::ahash::HashMap;
+
+macro_rules! iframe_style {
+    ($rect:expr) => {
+        format!(
+            "position: absolute; top: {}px; left: {}px; width: {}px; height: {}px; border: none; pointer-events: none;",
+            $rect.min.y,
+            $rect.min.x,
+            $rect.width(),
+            $rect.height()
+        )
+    };
+}
+
+pub struct TemplateApp {
+    iframe_handles: IframeHandles,
+}
+
+pub struct IframeHandles(HashMap<String, egui::Rect>);
+
+impl IframeHandles {
+    pub fn new() -> Self {
+        Self(HashMap::default())
+    }
+
+    pub fn create_or_update(&mut self, id: &str, url: &str, rect: egui::Rect) {
+        self.0.insert(id.to_string(), rect);
+        let element = web_sys::window()
+            .unwrap()
+            .document()
+            .unwrap()
+            .get_element_by_id(id);
+
+        if let Some(element) = element {
+            element
+                .set_attribute("style", &iframe_style!(rect))
+                .unwrap();
+        } else {
+            let window = web_sys::window().unwrap();
+            let document = window.document().unwrap();
+            let body = document.body().unwrap();
+            let iframe = document.create_element("iframe").unwrap();
+            iframe.set_attribute("id", id).unwrap();
+            iframe.set_attribute("src", url).unwrap();
+            iframe.set_attribute("style", &iframe_style!(rect)).unwrap();
+            body.append_child(&iframe).unwrap();
+        }
+    }
+}
 
 impl TemplateApp {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self
+        Self {
+            iframe_handles: IframeHandles::new(),
+        }
     }
 }
 
 impl eframe::App for TemplateApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::Window::new("Self coord").show(ctx, |ui| {
-            ui.vertical(|ui| {
-                let rect = ui.label("ref").rect;
-                ui.label(format!("{:?}", rect));
-                if ui.button("Open Iframe").clicked() {
-                    let window = web_sys::window().unwrap();
-                    let document = window.document().unwrap();
-                    let body = document.body().unwrap();
-                    let iframe = document.create_element("iframe").unwrap();
-                    iframe
-                        .set_attribute("src", "https://www.example.com")
-                        .unwrap();
-                    iframe.set_attribute("width", "100%").unwrap();
-                    iframe.set_attribute("height", "100%").unwrap();
-                    body.append_child(&iframe).unwrap();
-                }
-            });
+        egui::Window::new("Webview").show(ctx, |ui| {
+            let container = ui
+                .centered_and_justified(|ui| {
+                    ui.label("<web-iframe-content>");
+                })
+                .response;
+            self.iframe_handles.create_or_update(
+                "web-iframe-content",
+                "https://www.example.com",
+                container.rect,
+            );
         });
     }
 }
