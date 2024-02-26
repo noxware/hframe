27dd3b116b8e55fcde6fbd354e09d9445639e895
noxware <7684329+noxware@users.noreply.github.com>
2024-02-26 01:52:59 -0300

    update docs and readme

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,9 @@
 //!   it is not scaled. The default configuration of the [eframe_template](https://github.com/emilk/eframe_template) will work.
 //! - Currently the API only provides a way to create egui windows with HTML "inside"
 //!   but doesn't provide a way to put bare HTML content in other places.
+//! - Multiple masking strategies are provided bacause all browsers have problems with
+//!   one or the other. The `Auto` strategy tries to guess the best one for the current
+//!   browser but it may choose a wrong strategy for unknown browsers.
 
 mod aware;
 mod html_window;
