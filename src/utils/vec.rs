edefd5b097e6b734efc32d3cc63a8c7ca9628c1b
noxware <7684329+noxware@users.noreply.github.com>
2024-03-03 01:56:39 -0300

    remove some comments
    chat gpt stuff :P

--- src/utils/vec.rs
+++ src/utils/vec.rs
@@ -7,19 +7,16 @@ where
     I: PartialEq,
     F: FnMut(&T) -> I,
 {
-    // First, determine if and where a matching element exists
     let pos = v
         .iter()
         .position(|x| id_extractor(x) == id_extractor(&elem));
 
     match pos {
         Some(pos) => {
-            // If found, replace the element at that position
             let old_elem = std::mem::replace(&mut v[pos], elem);
             (&mut v[pos], Some(old_elem))
         }
         None => {
-            // If not found, push the new element to the end of the vector
             v.push(elem);
             let len = v.len();
             (&mut v[len - 1], None)
