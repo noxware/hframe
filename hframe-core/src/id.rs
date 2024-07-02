cbde4983543faec0e350f3c0c35a3e586a2b3c5e
noxware <7684329+noxware@users.noreply.github.com>
2024-07-01 22:48:07 -0300

    better debugging of ids while testing

--- hframe-core/src/id.rs
+++ hframe-core/src/id.rs
@@ -1,12 +1,31 @@
-use std::hash::{DefaultHasher, Hash, Hasher};
+use std::{
+    collections::HashMap,
+    fmt::{self, Debug, Formatter},
+    hash::{DefaultHasher, Hash, Hasher},
+    sync::{Mutex, OnceLock},
+};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+static DEBUG_ID_REGISTRY: OnceLock<Mutex<HashMap<Id, String>>> = OnceLock::new();
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub(crate) struct Id(u64);
 
 impl Id {
     /// A semantic id representing the root of a tree-like resource.
     pub(crate) fn root() -> Self {
         Id(42)
+    }
+
+    fn debug_name(&self) -> Option<String> {
+        DEBUG_ID_REGISTRY.get()?.lock().ok()?.get(self).cloned()
+    }
+
+    fn set_debug_name(&self, name: String) {
+        DEBUG_ID_REGISTRY
+            .get_or_init(|| Mutex::new(HashMap::new()))
+            .lock()
+            .unwrap()
+            .insert(*self, name);
     }
 }
 
@@ -21,8 +40,23 @@ impl From<&str> for Id {
         let mut hasher = DefaultHasher::new();
         id.hash(&mut hasher);
         let result = Id(hasher.finish());
-        eprintln!("Id::from({:?}) = {:?}", id, result);
+
+        // TODO: Allow debug names outside of tests.
+        if cfg!(test) {
+            result.set_debug_name(id.to_string());
+        }
+
         result
+    }
+}
+
+impl Debug for Id {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        if let Some(name) = self.debug_name() {
+            write!(f, "{}({})", self.0, name)
+        } else {
+            write!(f, "{}", self.0)
+        }
     }
 }
 
