4411b02de60075a0ff637e223f29ed87eb31f484
noxware <7684329+noxware@users.noreply.github.com>
2024-07-21 00:28:21 -0300

    too verbose

--- hframe-core/lib.js
+++ hframe-core/lib.js
@@ -1,18 +1,74 @@
-/** Main, mainly for scoping */
-function main() {
-  // When the mouse moves, adjust pointer events.
-  window.addEventListener("mousemove", (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
+const mousePosition = { x: 0, y: 0 };
 
-    document.querySelectorAll(".hframe-html-area").forEach((el) => {
-      // TODO: Use the information at the tree of rects to determine.
-    });
-  });
+window.addEventListener("mousemove", (e) => {
+  mousePosition.x = e.clientX;
+  mousePosition.y = e.clientY;
+});
+
+const maskTemplate = `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {width} {height}">
+  <defs>
+    <mask id="mask" x="0" y="0" width="{width}" height="{height}">
+      <rect x="0" y="0" width="{width}" height="{height}" fill="white" />
+      {holes}      
+    </mask>
+  </defs>
+  <rect x="{x}" y="{y}" width="{width}" height="{height}" fill="blue" mask="url(#mask)" />
+</svg>
+`;
+
+const holeTemplate = `
+<rect x="{x}" y="{y}" width="{width}" height="{height}" rx="5" fill="black" />
+`;
+
+const incomingMessages = [];
+const outgoingMessages = [];
+
+/** Exposes a way for WASM to send messages to JS. */
+export function sendMessage(message) {
+  incomingMessages.push(message);
 }
 
-main();
+/** Exposes a way for WASM to read messages from JS. Clears the messages. */
+export function receiveMessages() {
+  return outgoingMessages.splice(0, outgoingMessages.length);
+}
 
-export function doSomething() {
-  document.write("Hello, World!");
+/** Exposes a way for WASM to tell the JS side to do the work of a cycle,
+ *  processing all pending messages.
+ */
+export function tick() {
+  for (const message of incomingMessages) {
+    switch (message.type) {
+      case "TransformElement":
+        transformElement(message.id, message.rect, message.holes);
+        break;
+      case "Log":
+        console.log(message.message);
+        break;
+      default:
+        throw new Error(`Unknown message type: ${message.type}`);
+    }
+  }
+  incomingMessages.length = 0;
+}
+
+function transformElement(id, rect, holes) {
+  const el = document.getElementById(id);
+  const mask = maskTemplate
+    .replaceAll("{width}", rect.width)
+    .replaceAll("{height}", rect.height)
+    .replaceAll("{x}", rect.x)
+    .replaceAll("{y}", rect.y)
+    .replaceAll(
+      "{holes}",
+      holes.map((hole) =>
+        holeTemplate
+          .replaceAll("{width}", hole.width)
+          .replaceAll("{height}", hole.height)
+          .replaceAll("{x}", hole.x)
+          .replaceAll("{y}", hole.y)
+      )
+    );
+  el.style.mask = `url(data:image/svg+xml,${mask})`;
 }
--- hframe-core/src/main.rs
+++ hframe-core/src/main.rs
@@ -1,18 +1,105 @@
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq)]
+struct Pos {
+    x: f64,
+    y: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq)]
+struct Size {
+    width: f64,
+    height: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq)]
+struct Rect {
+    pos: Pos,
+    size: Size,
+}
+
+/// Operations that can be performed by the JS side.
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+enum OutgoingMessage {
+    /// Will mask/clip the given HTML element area.
+    TransformElement {
+        id: String,
+        rect: Rect,
+        holes: Vec<Rect>,
+    },
+    /// Request the current mouse position.
+    MousePositionRequest,
+    /// Logs to console.
+    Log { message: String },
+}
+
+impl OutgoingMessage {
+    /// Shorthand for sending the message to the JS side.
+    fn send(&self) {
+        send_message(serde_wasm_bindgen::to_value(self).unwrap());
+    }
+
+    /// Shorthand for calling `self.send()` and then `tick()`.
+    fn send_and_tick(&self) {
+        self.send();
+        tick();
+    }
+
+    /// Shorthand for calling `self.send()`, then `tick()` and then `IncomingMessage::receive()`.
+    fn send_and_tick_receiving(&self) -> IncomingMessage {
+        self.send();
+        tick();
+        IncomingMessage::receive()
+    }
+}
+
+/// Information received from the JS side.
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+enum IncomingMessage {
+    /// The mouse position received.
+    MousePositionResponse { x: f64, y: f64 },
+}
+
+impl IncomingMessage {
+    /// Deserializes the messages from a JS value.
+    fn from_js_value(value: JsValue) -> Self {
+        serde_wasm_bindgen::from_value(value).unwrap()
+    }
+
+    /// Receive a single message from the JS side.
+    /// Panics if there are no messages or if there are more than one messages.
+    fn receive() -> Self {
+        let messages = receive_messages();
+        let messages: Vec<IncomingMessage> = serde_wasm_bindgen::from_value(messages).unwrap();
+        assert_eq!(messages.len(), 1);
+        messages.into_iter().next().unwrap()
+    }
+}
 
 #[wasm_bindgen(module = "/lib.js")]
 extern "C" {
-    #[wasm_bindgen(js_name = doSomething)]
-    fn do_something();
-}
+    /// Let the JS side do some work, consuming messages.
+    #[wasm_bindgen]
+    fn tick();
 
-#[wasm_bindgen(start)]
-fn run() {
-    do_something();
+    /// Send a message to the JS side.
+    #[wasm_bindgen(js_name = sendMessage)]
+    fn send_message(msg: JsValue);
+
+    /// Receives all messages from the JS side.
+    #[wasm_bindgen(js_name = receiveMessages)]
+    fn receive_messages() -> JsValue;
 }
 
 fn main() {
     console_error_panic_hook::set_once();
 
-    do_something();
+    let IncomingMessage::MousePositionResponse { x, y } =
+        OutgoingMessage::MousePositionRequest.send_and_tick_receiving();
+
+    OutgoingMessage::Log {
+        message: format!("Mouse position: ({}, {})", x, y),
+    }
+    .send_and_tick();
 }
