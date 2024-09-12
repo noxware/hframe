4e490fd7538cc3ffb747021740f7cce3b9dfccfb
noxware <7684329+noxware@users.noreply.github.com>
2024-09-12 02:09:24 -0300

    doc

--- src/area.rs
+++ src/area.rs
@@ -1,10 +1,19 @@
+/// Represents a rectangular entity from the egui canvas or an html element, in unified way.
 pub(crate) struct Area {
     pub(crate) id: String,
+    /// The global x position of the area.
     pub(crate) x: f32,
+    /// The global y position of the area.
     pub(crate) y: f32,
+    /// The width of the area.
     pub(crate) width: f32,
+    /// The height of the area.
     pub(crate) height: f32,
+    /// The egui layer this area belongs to, obtained from `layer_id.id.value()`.
+    /// An egui area and an html area may share the same id if the first is the parent.
     pub(crate) layer_id: u64,
+    /// Is this area just a rectanble in egui/canvas, or it represents an html element to compose?
+    /// Html variant contains html specific information.
     pub(crate) kind: AreaKind,
 }
 
@@ -14,6 +23,8 @@ pub(crate) enum AreaKind {
 }
 
 pub(crate) struct AreaHtml {
+    /// If html element should exist in the dom but be invisible/minimized.
     pub(crate) visible: bool,
+    /// The html content of the area.
     pub(crate) content: String,
 }
