bacc38fbb8609335c56ae8cd87b31b696b8be07b
noxware <7684329+noxware@users.noreply.github.com>
2024-10-05 03:35:49 -0300

    svg on top with yew

--- src/companion.rs
+++ src/companion.rs
@@ -1,12 +1,12 @@
+use wasm_bindgen::JsCast;
 use yew::prelude::*;
 
 #[function_component]
 fn Companion() -> Html {
     html! {
-        <div>
-            <h1>{"Companion"}</h1>
-            <p>{"This is a companion component."}</p>
-        </div>
+        <svg style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1000; pointer-events: none;">
+            <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+        </svg>
     }
 }
 
@@ -15,7 +15,9 @@ pub fn install() {
     let document = window.unwrap().document().unwrap();
     let body = document.body().unwrap();
     let element = document.create_element("div").unwrap();
+    let element = element.dyn_into::<web_sys::HtmlElement>().unwrap();
+    element.style().set_property("display", "contents").unwrap();
     body.append_child(&element).unwrap();
 
-    yew::Renderer::<Companion>::with_root(element).render();
+    yew::Renderer::<Companion>::with_root(element.into()).render();
 }
