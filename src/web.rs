7e816ab26012e18901312e9365c87f3f20a59a11
noxware <7684329+noxware@users.noreply.github.com>
2024-10-06 23:08:55 -0300

    clear old experiments and move companion into src

--- src/web.rs
+++ src/web.rs
@@ -5,7 +5,7 @@ mod js {
     use serde::{de::DeserializeOwned, Serialize};
     use wasm_bindgen::prelude::*;
 
-    #[wasm_bindgen(module = "/companion-js-app/companion.js")]
+    #[wasm_bindgen(module = "/src/js/companion.js")]
     extern "C" {
         pub(crate) fn set_areas(areas: JsValue);
         pub(crate) fn run();
