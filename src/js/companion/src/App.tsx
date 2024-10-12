a6f323be1397bc3d51e97494e7200c81cc071ee9
noxware <7684329+noxware@users.noreply.github.com>
2024-10-12 15:56:20 -0300

    export a single es module js file from vite
    css assets svg assets also needed to be deleted

--- src/js/companion/src/App.tsx
+++ src/js/companion/src/App.tsx
@@ -1,20 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <a href="https://vitejs.dev" target="_blank"></a>
+        <a href="https://react.dev" target="_blank"></a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -29,7 +22,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/js/companion/vite.config.ts
+++ src/js/companion/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "src/main.tsx", // Your main entry file
+      // name: "Companion", // Global variable name for UMD/IIFE builds
+      formats: ["es"], // Output as ES module
+      fileName: "companion", // Output file name
+    },
+    rollupOptions: {
+      // If you want to include all dependencies in the bundle
+      external: [], // Empty array to bundle all dependencies
+      // Or specify external dependencies to exclude them from the bundle
+      // external: ['react', 'react-dom'],
+    },
+  },
+});
