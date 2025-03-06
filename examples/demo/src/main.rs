73e7b7415e21dcbb18a3716168f8a18195addbc7
noxware <7684329+noxware@users.noreply.github.com>
2025-03-06 18:44:12 -0300

    egui 0.31.1 as min version

--- examples/demo/src/main.rs
+++ examples/demo/src/main.rs
@@ -9,18 +9,45 @@ fn main() {
     //console_error_panic_hook::set_once();
 
     // Redirect `log` message to `console.log` and friends:
+
+    use eframe::wasm_bindgen::JsCast;
     eframe::WebLogger::init(log::LevelFilter::Debug).ok();
 
     let web_options = eframe::WebOptions::default();
 
     wasm_bindgen_futures::spawn_local(async {
-        eframe::WebRunner::new()
+        let document = eframe::web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<eframe::web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
             .start(
-                "the_canvas_id", // hardcode it
+                canvas,
                 web_options,
-                Box::new(|cc| Box::new(app::App::new(cc))),
+                Box::new(|cc| Ok(Box::new(app::App::new(cc)))),
             )
-            .await
-            .expect("failed to start eframe");
+            .await;
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
     });
 }
